822825e614ca05c4b608affc6ea14f12c8736f1c
Blake Ramsdell <blake.ramsdell@onelogin.com>
2018-04-23 16:54:35 -0700

    Added console app.

---
+++ Bcr.Fractions.CommandLine/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Bcr.Fractions;
+
+namespace Bcr.Fractions.CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(Expression.Evaluate(Console.ReadLine()));
+            }
+        }
+    }
+}
