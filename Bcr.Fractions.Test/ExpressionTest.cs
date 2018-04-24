7ac3a8bd5603f44a1ca72e5600cb230794af77d4
Blake Ramsdell <blake.ramsdell@onelogin.com>
2018-04-23 21:39:54 -0700

    Exception handling for bad input.

--- Bcr.Fractions.Test/ExpressionTest.cs
+++ Bcr.Fractions.Test/ExpressionTest.cs
@@ -48,5 +48,36 @@ namespace Bcr.Fractions.Test
             // Operands and operators shall be separated by one or more spaces.
             Assert.AreEqual("= 1_7/8", Expression.Evaluate("?     1/2      *     3_3/4"));
         }
+
+        // Now we start getting into the fuzzy area of "incorrect input". How
+        // do we try to predict what kind of cheese sandwich someone might
+        // enter and expect something good to happen.
+        //
+        // "Legal operators shall be *, /, +, - (multiply, divide, add,
+        // subtract)."
+        //
+        // What good does it do to tell the user "invalid operator"? It says
+        // in the instructions what the valid operators are. Why did they type
+        // an invalid operator? What if we complain about "invalid operator"
+        // and it's because they typed "? 3 1/2 * 1/4". Now we complained 
+        // about the operator, but what's really wrong is the format of the
+        // mixed number.
+        //
+        // Madness lies on this path.
+        //
+        // How much exception data should be presented? Can the user try and
+        // piece together what went wrong from a backtrace?
+        //
+        // And the only place we will end up is a complaint that the user has
+        // to interpret and act upon.
+        //
+        // So my opinion is to catch all the exceptions that fly out of
+        // Evaluate and say "Invalid input, please consult the instructions."
+
+        [TestMethod]
+        public void TestBadOperator()
+        {
+            Assert.AreEqual("Invalid input, please consult the instructions.", Expression.Evaluate("? 1/2 % 3_3/4"));
+        }
     }
 }
--- Bcr.Fractions/Expression.cs
+++ Bcr.Fractions/Expression.cs
@@ -4,42 +4,49 @@ namespace Bcr.Fractions
     {
         public static string Evaluate(string expression)
         {
-            var parsedExpression = System.Text.RegularExpressions.Regex.Split(expression, @"\s+");
-
-            // Expression syntax is:
-            //
-            // ? 1/2 * 3_3/4
-            //
-            // So [1] is term 1, [2] is operator and [3] is term 2
-
-            var term1 = Fraction.FromString(parsedExpression[1]);
-            var _operator = parsedExpression[2];
-            var term2 = Fraction.FromString(parsedExpression[3]);
-
-            Fraction result = null;
-
-            switch (_operator)
+            try
             {
-                case "*":
-                    result = term1 * term2;
-                    break;
+                var parsedExpression = System.Text.RegularExpressions.Regex.Split(expression, @"\s+");
 
-                case "+":
-                    result = term1 + term2;
-                    break;
+                // Expression syntax is:
+                //
+                // ? 1/2 * 3_3/4
+                //
+                // So [1] is term 1, [2] is operator and [3] is term 2
 
-                case "-":
-                    result = term1 - term2;
-                    break;
+                var term1 = Fraction.FromString(parsedExpression[1]);
+                var _operator = parsedExpression[2];
+                var term2 = Fraction.FromString(parsedExpression[3]);
 
-                case "/":
-                    result = term1 / term2;
-                    break;
+                Fraction result = null;
+
+                switch (_operator)
+                {
+                    case "*":
+                        result = term1 * term2;
+                        break;
+
+                    case "+":
+                        result = term1 + term2;
+                        break;
+
+                    case "-":
+                        result = term1 - term2;
+                        break;
+
+                    case "/":
+                        result = term1 / term2;
+                        break;
+                }
+
+                result.LowestTermify();
+
+                return $"= {result}";
             }
-
-            result.LowestTermify();
-
-            return $"= {result}";
+            catch
+            {
+                return "Invalid input, please consult the instructions.";
+            }
         }
     }
 }
